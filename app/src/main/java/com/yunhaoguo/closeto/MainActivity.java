b68b851430dbe8b3f492e09c454c5d64b15be4e5
gyhagithub <yg1578@nyu.edu>
2018-07-30 19:02:44 +0800

    first commit

---
+++ app/src/main/java/com/yunhaoguo/closeto/MainActivity.java
@@ -0,0 +1,21 @@
+package com.yunhaoguo.closeto;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        tv = (TextView) findViewById(R.id.tv);
+    }
+}
