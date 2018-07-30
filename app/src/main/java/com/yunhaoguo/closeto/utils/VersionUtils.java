80fd745fe62991bf605793dbc99d5aff50a55b53
gyhagithub <yg1578@nyu.edu>
2018-07-30 19:52:51 +0800

    add version封装

---
+++ app/src/main/java/com/yunhaoguo/closeto/utils/VersionUtils.java
@@ -0,0 +1,35 @@
+package com.yunhaoguo.closeto.utils;
+/*
+ * 项目名:     CloseTo
+ * 包名:      com.yunhaoguo.closeto.utils
+ * 文件名:     VersionUtils
+ * 创建者:     yunhaoguo
+ * 创建时间:    2018/7/30 下午7:36
+ * 描述:      版本管理
+ */
+
+
+import android.os.Build;
+
+public class VersionUtils {
+    //获取当前系统版本
+    public static int getVersion() {
+        return Build.VERSION.SDK_INT;
+    }
+
+    //是否大于5.0
+    public static boolean isLollipop() {
+        if (getVersion() >= Build.VERSION_CODES.LOLLIPOP) {
+            return true;
+        }
+        return false;
+    }
+
+    //是否大于6.0
+    public static boolean isM() {
+        if (getVersion() >= Build.VERSION_CODES.M) {
+            return true;
+        }
+        return false;
+    }
+}
