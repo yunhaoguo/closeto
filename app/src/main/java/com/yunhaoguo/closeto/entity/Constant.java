de0591bcbfd01c7da71a48a1ab7b4b43852e63e8
gyhagithub <yg1578@nyu.edu>
2018-08-01 18:32:37 +0800

    添加闪屏页

---
+++ app/src/main/java/com/yunhaoguo/closeto/entity/Constant.java
@@ -0,0 +1,16 @@
+package com.yunhaoguo.closeto.entity;
+/*
+ * 项目名:     CloseTo
+ * 包名:      com.yunhaoguo.closeto.entity
+ * 文件名:     Constant
+ * 创建者:     yunhaoguo
+ * 创建时间:    2018/8/1 下午4:49
+ * 描述:      常量
+ */
+
+
+public class Constant {
+
+    //闪屏页跳转到主页的handler所需的code
+    public static final int SPLASH_DELAY_CODE = 1001;
+}
--- app/src/main/java/com/yunhaoguo/closeto/ui/SplashActivity.java
+++ app/src/main/java/com/yunhaoguo/closeto/ui/SplashActivity.java
@@ -1,15 +1,65 @@
 package com.yunhaoguo.closeto.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Handler;
+import android.os.Message;
 
+import com.yunhaoguo.closeto.MainActivity;
 import com.yunhaoguo.closeto.R;
+import com.yunhaoguo.closeto.base.BasePermissionActivity;
+import com.yunhaoguo.closeto.entity.Constant;
+import com.yunhaoguo.closeto.utils.SharedPrefsUtils;
+import com.yunhaoguo.closeto.utils.VersionUtils;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends BasePermissionActivity {
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case Constant.SPLASH_DELAY_CODE:
+                    if (isFirstRunApp()) {
+                        startActivity(new Intent(SplashActivity.this, GuideActivity.class));
+                    } else {
+                        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    }
+            }
+        }
+    };
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        initPermissions();
+
+        initView();
+    }
+
+    //动态权限申请
+    private void initPermissions() {
+        if (VersionUtils.isM()) {
+            //askPermissions();
+        }
+    }
+    //加载界面
+    private void initView() {
+        mHandler.sendEmptyMessageDelayed(Constant.SPLASH_DELAY_CODE, 2000);
+    }
+
+    //判断是否第一次运行
+    private boolean isFirstRunApp() {
+        boolean isFirst = SharedPrefsUtils.getBoolean(this, "is_first", true);
+        if (isFirst) {
+            SharedPrefsUtils.putBoolean(this, "is_first", false);
+            return true;
+        } else {
+            return false;
+        }
     }
 }
